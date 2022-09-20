bf9dda48ce3cb42e06f507779bac9e5bec99be2b
AhmedElemary57 <honor5766@gmail.com>
2022-09-20 22:23:28 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MakeTransactionComponent} from "./components/make-transaction/make-transaction.component";
+import {HomePageComponent} from "./components/home-page/home-page.component";
+import {CustomerListComponent} from "./components/customer-list/customer-list.component";
+import {CustomerComponent} from "./components/customer/customer.component";
+import {TransactionsComponent} from "./components/transactions/transactions.component";
+import {TransactionItemComponent} from "./components/transaction-item/transaction-item.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'customers', component: CustomerListComponent },
+  { path: 'customers/:id', component: CustomerComponent },
+  { path: 'transactions', component: TransactionsComponent },
+  { path: 'transactions/:id', component: TransactionItemComponent },
+  { path: 'make-transaction', component: MakeTransactionComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TransactionItemComponent } from './components/transaction-item/transaction-item.component';
+import { MakeTransactionComponent } from './components/make-transaction/make-transaction.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomePageComponent,
+    CustomerListComponent,
+    CustomerComponent,
+    TransactionsComponent,
+    TransactionItemComponent,
+    MakeTransactionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
